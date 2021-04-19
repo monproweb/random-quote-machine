9f3cdc1fffcc58f7caa1ea5a49b5b69ee703bea7
Thomas Erhel <thomas.erhel@gmail.com>
2021-04-19 12:01:12 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable max-len */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import $ from 'jquery';
+import './App.scss';
+
+let quotesData: { quotes: string | any[]; };
+
+var currentQuote = '',
+  currentAuthor = '';
 
 function App() {
+  function getQuotes() {
+    return $.ajax({
+      headers: {
+        Accept: 'application/json'
+      },
+      url:
+        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+      success: function (jsonQuotes) {
+        if (typeof jsonQuotes === 'string') {
+          quotesData = JSON.parse(jsonQuotes);
+          console.log('quotesData');
+          console.log(quotesData);
+        }
+      }
+    });
+  }
+
+  function getRandomQuote() {
+    return quotesData.quotes[
+      Math.floor(Math.random() * quotesData.quotes.length)
+    ];
+  }
+
+  function getQuote() {
+    let randomQuote = getRandomQuote();
+
+    currentQuote = randomQuote.quote;
+    currentAuthor = randomQuote.author;
+
+    $('#tweet-quote').attr(
+      'href',
+      'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+    );
+
+    $('#tumblr-quote').attr(
+      'href',
+      'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+    );
+
+    $('.quote-text').animate({ opacity: 0 }, 500, function () {
+      $(this).animate({ opacity: 1 }, 500);
+      $('#text').text(randomQuote.quote);
+    });
+
+    $('.quote-author').animate({ opacity: 0 }, 500, function () {
+      $(this).animate({ opacity: 1 }, 500);
+      $('#author').html(randomQuote.author);
+    });
+  }
+
+  $(document).ready(function () {
+    getQuotes().then(() => {
+      getQuote();
+    });
+
+    $('#new-quote').on('click', getQuote);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="quote-box">
+        <div className="quote-text">
+          <i className="fa fa-quote-left"> </i><span id="text"></span>
+        </div>
+        <div className="quote-author">- <span id="author"></span></div>
+        <div className="buttons">
+          <a
+            className="button"
+            id="tweet-quote"
+            title="Tweet this quote!"
+            target="_top"
+          >
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a
+            className="button"
+            id="tumblr-quote"
+            title="Post this quote on tumblr!"
+            target="_blank"
+          >
+            <i className="fa fa-tumblr"></i>
+          </a>
+          <button className="button" id="new-quote">New quote</button>
+        </div>
+      </div>
+      <div className="footer">by <a href="https://monproweb.io">Mon Pro Web</a></div>
     </div>
   );
 }
